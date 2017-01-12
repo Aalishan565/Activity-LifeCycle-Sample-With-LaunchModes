fe6fc15b4994f346929a41b2fa3e5005f4e36c9b
satishest <satish.telasinge@techendeavour.com>
2017-01-12 14:58:02 +0530

    -added log

--- app/src/main/java/com/activitylifecyclesamplepoc/ActivityB.java
+++ app/src/main/java/com/activitylifecyclesamplepoc/ActivityB.java
@@ -5,23 +5,25 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class ActivityB extends AppCompatActivity {
-    String TAG = "ActivityB";
+    String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+        Log.d(TAG, "onCreate ActivityB");
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart");
+        Log.d(TAG, "onStart ActivityB");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume");
+        Log.d(TAG, "onResume ActivityB");
         // finish();
 
     }
@@ -29,24 +31,24 @@ public class ActivityB extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause");
+        Log.d(TAG, "onPause ActivityB");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop");
+        Log.d(TAG, "onStop ActivityB");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "onRestart");
+        Log.d(TAG, "onRestart ActivityB");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy");
+        Log.d(TAG, "onDestroy ActivityB");
     }
 }
--- app/src/main/java/com/activitylifecyclesamplepoc/MainActivity.java
+++ app/src/main/java/com/activitylifecyclesamplepoc/MainActivity.java
@@ -7,31 +7,30 @@ import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    String TAG = "MainActivity";
+    String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate");
+        Log.d(TAG, "onCreate MainActivity");
     }
 
     public void onClick(View view) {
         startActivity(new Intent(this, ActivityB.class));
-      
-    }
 
+    }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart");
+        Log.d(TAG, "onStart MainActivity");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume");
+        Log.d(TAG, "onResume MainActivity");
         // finish();
 
     }
@@ -39,24 +38,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause");
+        Log.d(TAG, "onPause MainActivity");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop");
+        Log.d(TAG, "onStop MainActivity");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "onRestart");
+        Log.d(TAG, "onRestart MainActivity");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy");
+        Log.d(TAG, "onDestroy MainActivity");
     }
 }
