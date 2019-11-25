60bbf81dc55da1cb8e1ccb7e59a1323508f30eaa
Aalishan Ansari <aalishan565@gmail.com>
2019-11-25 13:25:19 +0530

    -added some method

--- app/src/main/java/com/activitylifecyclesamplepoc/ActivityC.java
+++ app/src/main/java/com/activitylifecyclesamplepoc/ActivityC.java
@@ -3,19 +3,60 @@ package com.activitylifecyclesamplepoc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class ActivityC extends AppCompatActivity {
+    String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c);
+        Log.d(TAG, "onCreate ActivityC");
     }
 
     public void onClick(View view) {
-        //finish();
-         startActivity(new Intent(this, ActivityD.class));
+        //  finish();
+        startActivity(new Intent(this, ActivityD.class));
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart ActivityC");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume ActivityC");
+        // finish();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause ActivityC");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop ActivityC");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart ActivityC");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy ActivityC");
     }
 }
--- app/src/main/java/com/activitylifecyclesamplepoc/ActivityD.java
+++ app/src/main/java/com/activitylifecyclesamplepoc/ActivityD.java
@@ -3,19 +3,62 @@ package com.activitylifecyclesamplepoc;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class ActivityD extends AppCompatActivity {
+
+    String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_d);
+        Log.d(TAG, "onCreate ActivityD");
     }
 
     public void onClick(View view) {
         //  finish();
-        startActivity(new Intent(this, ActivityB.class));
+        startActivity(new Intent(this, ActivityD.class));
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart ActivityD");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume ActivityD");
+        // finish();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause ActivityD");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop ActivityD");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart ActivityD");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy ActivityD");
+    }
 }
+
